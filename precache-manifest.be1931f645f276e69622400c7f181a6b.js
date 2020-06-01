c58ec614ae48cdba448b304bd2b9911635f0d787
Azure Pipelines <SpeedTechSolutions@visualstudio.com>
2020-06-01 05:48:53 +0000

    Release Version 2020.06.01.352

---
+++ precache-manifest.be1931f645f276e69622400c7f181a6b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ecde2dcf3102139dafd0659552d1d42",
+    "url": "/TodoApp.ReactHooks/index.html"
+  },
+  {
+    "revision": "e6dfa24396c260fa7538",
+    "url": "/TodoApp.ReactHooks/static/css/2.cff28e81.chunk.css"
+  },
+  {
+    "revision": "e6dfa24396c260fa7538",
+    "url": "/TodoApp.ReactHooks/static/js/2.25dd7c9c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/TodoApp.ReactHooks/static/js/2.25dd7c9c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9f29016cfa9741e3b8c0",
+    "url": "/TodoApp.ReactHooks/static/js/main.85889ddd.chunk.js"
+  },
+  {
+    "revision": "61c1c3a7c8b2cd6cf790",
+    "url": "/TodoApp.ReactHooks/static/js/runtime-main.db3b0680.js"
+  }
+]);
